4cde56368527440c0636e0fc70ed4fec45676002
Sashka <Sashka>
2021-01-19 11:36:57 +0300

    init

---
+++ src/main/java/com/itechart/juiceplus/JuiceplusApplication.java
@@ -0,0 +1,13 @@
+package com.itechart.juiceplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuiceplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuiceplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itechart/juiceplus/JuiceplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.juiceplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuiceplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
